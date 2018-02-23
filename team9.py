db476f6663c3d7e8ad72e40559dcb6a25fd2fdd6
PrimoRazor <36012974+PrimoRazor@users.noreply.github.com>
2018-02-23 14:34:13 -0700

    needs improvment
    Anthony Herrmann
    Calub beethall

--- team9.py
+++ team9.py
@@ -11,12 +11,21 @@ strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+    if len(my_history) == 0:
+        return 'c'
+    elif len(my_history):
+        return 'c'
+    elif len(my_history):
+        return 'b'
+    elif their_history[-1] == 'c':
+        return 'b'
+    elif their_history[-1] == 'b':
+        return 'c'
+    else:
+        return 'c'
+        if len (my_score)==-2000:
+            return 'b'
+
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
